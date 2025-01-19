332ae0529dcc09e7a00ba6bb8c384d6cb44e610c
antllcon <pokemonivan32@gmail.com>
2025-01-19 13:55:13 +0300

    mvp-max Релиз игры, версия 1.3 - исправлены баги (нет рестарта)

--- cmake-build-debug/CMakeFiles/pixel_poem.dir/core/game/GameStateManager.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/pixel_poem.dir/core/game/GameStateManager.cpp.o: \
- /projects/pixel_poem/core/game/GameStateManager.cpp \
- /usr/include/stdc-predef.h \
- /projects/pixel_poem/core/game/GameStateManager.h
